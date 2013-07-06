5a853ee5a76a29fdf3433f6bffc7457d777aef35
vinit kumar <vinit.kumar@changer.nl>
2013-07-07 04:32:22 +0530

    fixing setup.py and gitignore

--- setup.py
+++ setup.py
@@ -1,58 +1,17 @@
-#! /usr/bin/env python
+from setuptools import setup, find_packages
 
-import os
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-import json2xml
-
-
-
-def publish():
-    """Publish to PyPi"""
-    os.system("python setup.py sdist upload")
-
-if sys.argv[-1] == "publish":
-    publish()
-    sys.exit()
-
-required = ['dict2xml']
+version = '0.6'
 
 setup(
     name='json2xml',
-    version=json2xml.__version__,
-    description='Python Command-line Application Tools',
-    long_description=open('README.md').read() + '\n\n' +
-                     open('HISTORY.md').read(),
+    version=version,
+    description='To covert json data to xml data',
     author='Vinit Kumar',
     author_email='vinit.kumar@changer.nl',
-    url='https://github.com/vinitcool76/json2xml',
-    data_files=[
-        'readme.md',
-        'history.md',
+    url='http://github.com:vinitcool76/json2xml.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
     ],
-    packages=[
-        json2xml,
-    ],
-    install_requires=required,
-    license='MIT',
-    classifiers=(
-#       'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Topic :: Terminals :: Terminal Emulators/X Terminals',
-    ),
 )

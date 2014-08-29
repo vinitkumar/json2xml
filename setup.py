ba4d4e4f26c92725edf521b436098a16b1c07ad0
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2014-08-29 11:05:27 +0530

    fix dependencies issue

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 version = '0.6'
 
@@ -12,6 +13,6 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[
-    ],
+    install_requires=open(os.path.join(os.path.dirname(__file__),
+         'requirements.txt')).read().split(),
 )

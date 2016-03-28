057c56606cfe746c799953b0b23996d3a7b7bbd5
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-03-29 00:46:05 +0530

    fixes #8, enable python2 and 3 support

--- json2xml/json2xml.py
+++ json2xml/json2xml.py
@@ -1,27 +1,30 @@
 #! /usr/bin/env python
+import requests
+import simplejson
+import urllib
+import dict2xml
+from bs4 import BeautifulSoup
 
-import simplejson, urllib, dict2xml
-from BeautifulSoup import BeautifulStoneSoup
 
 class Json2xml(object):
-    
+
     # -------------------------------
-    ## 
+    ##
     # @Synopsis  This class could read a json file
     # from the filesystem or get a file from across
-    # the Internet, and convert that json object to 
+    # the Internet, and convert that json object to
     # xml
-    # 
+    #
     # @Param data : Data to be fed into the system.
-    # 
+    #
     # @Returns  Null
     # ---------------------------------
     def __init__(self, data):
-       self.data = data
+        self.data = data
 
     # -------------------------------
-    ## 
-    # @Synopsis  Read JSON from a file in 
+    ##
+    # @Synopsis  Read JSON from a file in
     # the system
     # ---------------------------------
     @classmethod
@@ -30,35 +33,31 @@ class Json2xml(object):
             json_data = open(filename)
             data = simplejson.load(json_data)
         except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            print ("I/O error({0}): {1}".format(e.errno, e.strerror))
             data = []
         return cls(data)
 
     # -------------------------------
-    ## 
+    ##
     # @Synopsis  Fetches the JSON
     # data from an URL Source.
     #
     # ---------------------------------
     @classmethod
     def fromurl(cls, url):
-        data = simplejson.load(urllib.urlopen(url))
+        data = requests.get(url).json()
         return cls(data)
-    
+
     # -------------------------------
-    ## 
+    ##
     # @Synopsis  This method actually
-    # converts the json data that is converted 
+    # converts the json data that is converted
     # into dict into XML
-    # 
-    # @Returns XML 
+    #
+    # @Returns XML
     # ---------------------------------
     def json2xml(self):
         if self.data:
             xmldata = dict2xml.dict2xml(self.data)
-            xml = BeautifulStoneSoup(xmldata)
+            xml = BeautifulSoup(xmldata, "html.parser")
             return xml
-
-            
-
-

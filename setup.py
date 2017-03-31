2e71cae8cbdecf021e43686287461713d58081d9
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-03-31 16:47:05 +0530

    update info

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2.3'
+version = '1.2.5'
 
 setup(
     name='json2xml',
     version=version,
-    description='To convert json data to xml data',
+    description='A simple python package to convert json to xml data',
     author='Vinit Kumar',
-    author_email='vinit.kumar@changer.nl',
+    author_email='vinit1414.08@bitmesra.ac.in',
     url='https://github.com/vinitkumar/json2xml',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=['BeautifulSoup4==4.4.1',
-                      'dict2xml==1.3',
-                      'simplejson==3.6.5',
+    install_requires=['BeautifulSoup4==4.5.3',
+                      'dict2xml==1.5',
+                      'simplejson==3.10.0',
                       'six==1.10.0',
-                      'lxml',
-                      'requests',
+                      'lxml==3.7.3',
+                      'requests==2.13.0',
                       ],
 )

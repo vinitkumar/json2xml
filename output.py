3f6bcc04ab1108badf0f76693bb1bed0c82ea191
Vinit Kumar <mail@vinitkumar.me>
2019-01-18 18:41:05 +0530

    cleanup

--- output.py
+++
@@ -1,4 +0,0 @@
-from src.json2xml import Json2xml
-data = Json2xml.fromjsonfile('examples/example.json').data
-data_object = Json2xml(data, wrapper="custom", indent=4)
-print(data_object.json2xml()) #xml output

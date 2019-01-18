377df0e833ce640adac17d4dbbd738696c70d4b5
Vinit Kumar <mail@vinitkumar.me>
2019-01-18 17:56:37 +0530

    some cleanup and update the tests

--- src/json2xml.py
+++ src/json2xml.py
@@ -15,28 +15,31 @@ class Json2xml(object):
     xml
     """
 
-    def __init__(self, data: str) -> None:
+    def __init__(self, data: str, wrapper:str = "all", indent:int=4) -> None:
         self.data = data
-
+        self.indent = indent
+        self.wrapper = wrapper
 
     @classmethod
     def fromjsonfile(cls, filename: str):
         """
-        Read JSON from a file in
-        the system
+        read json from a json file in the filesystem
 
         :param filename:
         :return: dict
         """
+        data = []
         try:
             json_data = open(filename)
             data = json.load(json_data)
             json_data.close()
+        except ValueError:
+            print("the file doesn't appear to be a valid json")
         except IOError as e:
+            print("looks like the file doesn't exists")
             print("I/O error({0}): {1}".format(e.errno, e.strerror))
             data = []
         return cls(data)
-
 
     @classmethod
     def fromurl(cls, url: str, params=None):
@@ -64,11 +67,13 @@ class Json2xml(object):
         :return: dict
         """
         if type(data) is not str:
-            raise "Sorry! it doesn't seem to be valid string"
+            raise ValueError("the input doesn't seem to be valid string")
         try:
             data = json.loads(data)
+        except ValueError:
+            print("the string doesn't appear to be a valid json")
         except Exception as e:
-            print("Sorry, failed to load json, the JSON isn't right")
+            print("the string doesn't appear to be a valid json", e)
             data = []
         return cls(data)
 
@@ -80,4 +85,4 @@ class Json2xml(object):
         :return: XML string
         """
         if self.data:
-            return dict2xml.dict2xml(self.data, wrap="all", indent="  ")
+            return dict2xml.dict2xml(self.data, self.wrapper , indent=self.indent * ' ')
--- tests/test.py
+++ tests/test.py
@@ -15,7 +15,7 @@ from src.json2xml import Json2xml
 
 
 class Json2xmlTestCase(unittest.TestCase):
-    def test_is_json_from_file_works(self):
+    def test_json_from_file_works(self):
         data = Json2xml.fromjsonfile('examples/example.json').data
         data_object = Json2xml(data)
         xml_output = data_object.json2xml()
@@ -23,6 +23,26 @@ class Json2xmlTestCase(unittest.TestCase):
         # since it's a valid XML, xml to dict is able to load it and return
         # elements from under the all tag of xml
         self.assertTrue(type(dict_from_xml['all']) == OrderedDict)
+
+    def test_json_from_url(self):
+        data = Json2xml.fromurl('https://coderwall.com/vinitcool76.json').data
+        data_object = Json2xml(data)
+        xml_output = data_object.json2xml()
+        dict_from_xml = xmltodict.parse(xml_output)
+        # since it's a valid XML, xml to dict is able to load it and return
+        # elements from under the all tag of xml
+        self.assertTrue(type(dict_from_xml['all']) == OrderedDict)
+
+    def test_json_from_string(self):
+        data = Json2xml.fromstring('{"login":"mojombo","id":1,"avatar_url":"https://avatars0.githubusercontent.com/u/1?v=4"}').data
+        data_object = Json2xml(data)
+        xml_output = data_object.json2xml()
+        dict_from_xml = xmltodict.parse(xml_output)
+        print('keys', dict_from_xml)
+        # since it's a valid XML, xml to dict is able to load it and return
+        # elements from under the all tag of xml
+        self.assertTrue(type(dict_from_xml['all']) == OrderedDict)
+
 
 
 if __name__ == '__main__':

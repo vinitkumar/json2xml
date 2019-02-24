399a6d710135abdcef9219d5a32597ad311b4933
Vinit Kumar <mail@vinitkumar.me>
2019-02-24 14:57:10 +0530

    update requirements and setuptools

--- setup.py
+++ setup.py
@@ -5,42 +5,42 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [open("requirements_prod.txt").read()]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Vinit Kumar",
-    author_email='mail@vinitkumar.me',
+    author_email="mail@vinitkumar.me",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Simple Python Library to convert JSON to XML",
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='json2xml',
-    name='json2xml',
-    packages=find_packages(include=['json2xml']),
+    keywords="json2xml",
+    name="json2xml",
+    packages=find_packages(include=["json2xml"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/vinitkumar/json2xml',
-    version='3.0.0',
+    url="https://github.com/vinitkumar/json2xml",
+    version="3.0.0",
     zip_safe=False,
 )
---
+++ test.py
@@ -0,0 +1,5 @@
+from json2xml.json2xml import Json2xml
+
+
+data = Json2xml.readfromjson("~/Downloads/data.json").data
+print(data.json2xml())

b214269ffde8930c5427bbb93fecd4c39956a293
Vinit Kumar <mail@vinitkumar.me>
2019-02-25 18:29:52 +0530

    test: add testcases for the different utils method
    Now, since the methods are separated, unit tests are added to check
    if they work properly and if they return the correct type and also, the
    if they exit properly incase the input is invalid
    
    - Github Issue: #39
    
    Authored-by: Vinit Kumar <mail@vinitkumar.me>
    Signed-off-by: Vinit Kumar <mail@vinitkumar.me>

--- tests/test_json2xml.py
+++ tests/test_json2xml.py
@@ -5,6 +5,7 @@
 
 
 import unittest
+import pytest
 
 from json2xml import json2xml, readfromjson, readfromstring, readfromurl
 
@@ -23,12 +24,34 @@ class TestJson2xml(unittest.TestCase):
         data = readfromjson("examples/licht.json")
         assert type(data) is dict
 
+    def test_read_from_invalid_json(self):
+        """Test something."""
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            data = readfromjson("examples/licht_wrong.json")
+        assert pytest_wrapped_e.type == SystemExit
+
     def test_read_from_url(self):
         data = readfromurl("https://coderwall.com/vinitcool76.json")
         assert type(data) is dict
 
-    def test_read_from_string(self):
+    def test_read_from_wrong_url(self):
+        """
+        Use wrong url and check if there is a sytemExit
+        """
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            data = readfromurl("https://coderwall.com/vinitcool76.jsoni")
+        print("type is ", pytest_wrapped_e.type)
+        assert pytest_wrapped_e.type == SystemExit
+
+    def test_read_from_jsonstring(self):
         data = readfromstring(
             '{"login":"mojombo","id":1,"avatar_url":"https://avatars0.githubusercontent.com/u/1?v=4"}'
         )
         assert type(data) is dict
+
+    def test_read_from_invalid_jsonstring(self):
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            data = readfromstring(
+                '{"login":"mojombo","id":1,"avatar_url":"https://avatars0.githubusercontent.com/u/1?v=4"'
+            )
+        assert pytest_wrapped_e.type == SystemExit

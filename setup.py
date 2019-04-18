f8b7c77dd5d73a026c871cfe83ae1939144f1e0d
Vinit Kumar <mail@vinitkumar.me>
2019-04-18 11:27:48 +0530

    chore: bump version

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinitkumar/json2xml",
-    version="3.0.0",
+    version="3.0.1",
     zip_safe=False,
 )

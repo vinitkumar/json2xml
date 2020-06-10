3ec625ef465ade108563735bde4fd6cb767e4e68
KeiroS <keirospl@gmail.com>
2020-06-10 04:27:49 +0200

    Update json2xml.py (#56)
    adding attr_type attribute to disable type attributes

--- json2xml/json2xml.py
+++ json2xml/json2xml.py
@@ -7,18 +7,20 @@ class Json2xml:
     def __init__(
             self, data: str,
             wrapper: str = "all",
-            pretty: bool = True
+            pretty: bool = True,
+            attr_type: bool = True
     ):
         self.data = data
         self.pretty = pretty
         self.wrapper = wrapper
+        self.attr_type = attr_type
 
     def to_xml(self):
         """
         Convert to xml using dicttoxml.dicttoxml and then pretty print it.
         """
         if self.data:
-            xml_data = dicttoxml.dicttoxml(self.data, custom_root=self.wrapper)
+            xml_data = dicttoxml.dicttoxml(self.data, custom_root=self.wrapper, attr_type=self.attr_type)
             if self.pretty:
                 return parseString(xml_data).toprettyxml()
             return xml_data

06c8e11268979dc071a195af7f644726fdf92545
Vinit Kumar <mail@vinitkumar.me>
2021-09-10 23:00:00 +0530

    :bookmark: feat: bump version

--- json2xml/__init__.py
+++ json2xml/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Vinit Kumar"""
 __email__ = "mail@vinitkumar.me"
-__version__ = "3.7.0b1"
+__version__ = "3.7.0b2"
 
 
 # from .utils import readfromurl, readfromstring, readfromjson

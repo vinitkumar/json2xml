17f31dc457b3fc079237f4359b7be38e332f8b4d
Vinit Kumar <mail@vinitkumar.me>
2021-09-11 13:13:17 +0530

    :bookmark: feat: final release for v3.7.0

--- json2xml/__init__.py
+++ json2xml/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Vinit Kumar"""
 __email__ = "mail@vinitkumar.me"
-__version__ = "3.7.0b2"
+__version__ = "3.7.0"
 
 
 # from .utils import readfromurl, readfromstring, readfromjson

28de9848ba4572b65eac1cf7b4fa2516079adad8
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2023-08-22 01:36:15 +0530

    fix: tests

--- tests/test_json2xml.py
+++ tests/test_json2xml.py
@@ -34,7 +34,7 @@ class TestJson2xml:
         data = readfromjson("examples/bigexample.json")
         if isinstance(data, list):
             # it's json array, so we just take the first element and check it's type
-            assert isinstance(data[0], dict)
+            assert type(data[0])  == dict
         else:
             data = readfromjson("examples/licht.json")
             assert isinstance(data, dict)

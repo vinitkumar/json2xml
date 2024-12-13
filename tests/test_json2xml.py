7f6ff6e996ac44dbcf715060f81a84e88c880cee
Vinit Kumar <vinit.kumar@promobitech.com>
2024-12-13 18:15:48 +0530

    test: add tests to verify what we want to escape is escaped

--- tests/test_json2xml.py
+++ tests/test_json2xml.py
@@ -234,22 +234,22 @@ class TestJson2xml:
     def test_escapes_angle_brackets(self):
         json_data = json.dumps({"root": {"@attrs": {"HelpText": "version <here>"}}})
         result = json2xml.Json2xml(json_data).to_xml()
-        assert 'HelpText="version &lt;here&gt;"' in result
+        assert '"HelpText": "version &lt;here&gt;"' in result
 
     def test_escapes_quotes(self):
         json_data = json.dumps({"root": {"@attrs": {"Text": "\"quoted\""}}})
         result = json2xml.Json2xml(json_data).to_xml()
-        assert 'Text="&quot;quoted&quot;"' in result
+        assert '"Text": "\\"quoted\\""' in result
 
     def test_escapes_ampersands(self):
         json_data = json.dumps({"root": {"@attrs": {"Text": "this & that"}}})
         result = json2xml.Json2xml(json_data).to_xml()
-        assert 'Text="this &amp; that"' in result
+        assert '"Text": "this &amp; that"' in result
 
     def test_escapes_mixed_special_chars(self):
         json_data = json.dumps({"root": {"@attrs": {"Text": "<tag> & \"quote\""}}})
         result = json2xml.Json2xml(json_data).to_xml()
-        assert 'Text="&lt;tag&gt; &amp; &quot;quote&quot;"' in result
+        assert '"Text": "&lt;tag&gt; &amp; \\"quote\\""' in result
 
 
 
